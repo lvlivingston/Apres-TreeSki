f8bafec8d30bb15ca20d3ebd04bdcf3dfbf98923
lvlivingston <lvlivingston@gmail.com>
2023-10-06 18:57:03 -0500

    initial js setup

---
+++ script.js
@@ -0,0 +1,35 @@
+/* ----- DOM SELECTORS ------ */
+const movement = document.querySelector("#movement");
+const status = document.querySelector("#status");
+const canvas = document.querySelector("canvas");
+
+/* ----- CANVAS SETUP ------- */
+const ctx = canvas.getContext("2d");
+console.log(ctx);
+canvas.setAttribute("height", getComputedStyle(canvas).height);
+canvas.setAttribute("width", getComputedStyle(canvas).width);
+
+/* ----- CLASSES ------------ */
+class Player {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.enoughTime = true;
+    }
+    render() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+const skier = new Player(100, 50, 30, 70, "#217224");
+
+
+
+/* ----- FUNCTIONS ---------- */
+
+
+/* ----- EVENT LISTENERS ---- */
