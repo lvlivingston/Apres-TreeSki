2daec1c8ca89a6518d91cecaf765bd5a45a908ca
lvlivingston <lvlivingston@gmail.com>
2023-10-08 14:36:25 -0500

    rendered skier with colors and movement

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 /* ----- DOM SELECTORS ------ */
 const movement = document.querySelector("#movement");
-const status = document.querySelector("#status");
 const canvas = document.querySelector("canvas");
 
 /* ----- CANVAS SETUP ------- */
 const ctx = canvas.getContext("2d");
-console.log(ctx);
 canvas.setAttribute("height", getComputedStyle(canvas).height);
 canvas.setAttribute("width", getComputedStyle(canvas).width);
+
+
+
+
 
 /* ----- CLASSES ------------ */
 class Player {
@@ -24,13 +26,63 @@ class Player {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
-const testPlayer = new Player(45, 45, 65, 23, "green");
-testPlayer.render();
-const skier = new Player(100, 50, 30, 70, "#217224");
+
+const skier = new Player(50, 50, 30, 70, "blue");
+console.log(skier);
 
 
 
 /* ----- FUNCTIONS ---------- */
+function drawBox(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
 
+const currentlyPressedKeys = {}
+function movementHandler() {
+    const speed = 10;
+    if (currentlyPressedKeys["i"]) {
+        let isDiagnal = false;
+        if (currentlyPressedKeys["j"] || currentlyPressedKeys["l"]) {
+            isDiagnal = true;
+        }
+        skier.y -= isDiagnal ? speed * .75 : speed;
+    } 
+    if (currentlyPressedKeys["k"]) {
+        let isDiagnal = false;
+        if (currentlyPressedKeys["j"] || currentlyPressedKeys["l"]) {
+            isDiagnal = true;
+        }
+        skier.y += isDiagnal ? speed * .75 : speed;
+    } 
+    if (currentlyPressedKeys["j"]) {
+        let isDiagnal = false;
+        if (currentlyPressedKeys["i"] || currentlyPressedKeys["l"]) {
+            isDiagnal = true;
+        }
+        skier.x -= isDiagnal ? speed * .75 : speed;
+    } 
+    if (currentlyPressedKeys["l"]) {
+        let isDiagnal = false;
+        if (currentlyPressedKeys["i"] || currentlyPressedKeys["k"]) {
+            isDiagnal = true;
+        }
+        skier.x += isDiagnal ? speed * .75 : speed;
+    } 
+}
+
+const gameInterval = setInterval(gameloop, 80);
+function gameloop() {
+    movementHandler();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    skier.render();
+}
 
 /* ----- EVENT LISTENERS ---- */
+canvas.addEventListener('click', e => {
+    movement.innerText = `x: ${e.offsetX}, y: ${e.offsetY}`;
+    drawBox(e.offsetX, e.offsetY, 30, 30, "#C724B1")
+});
+
+document.addEventListener('keydown', e => currentlyPressedKeys[e.key] = true);
+document.addEventListener('keyup', e => currentlyPressedKeys[e.key] = false);
