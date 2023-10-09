24767bbd8c7ce4e2cffd1846671236b6d55949b4
lvlivingston <lvlivingston@gmail.com>
2023-10-09 14:36:37 -0500

    train station renders at 0:05

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const movement = document.querySelector("#movement");
 const startButton = document.getElementById("startButton");
 const canvas = document.querySelector("canvas");
+const counterElement = document.getElementById("counter");
+const timerElement = document.getElementById("timer");
 
 /* ----- CANVAS SETUP ------- */
 const ctx = canvas.getContext("2d");
@@ -11,9 +13,10 @@ canvas.setAttribute("width", getComputedStyle(canvas).width);
 
 
 
-
 /* ----- CLASSES ------------ */
+let gameStarted = false;
 let enoughTime = true;
+let timeRemaining = 60;
 
 class Player {
     constructor(x, y, width, height, color) {
@@ -41,9 +44,18 @@ const trees = [
     new Player(Math.random() * canvas.width - 25, Math.random() * canvas.height, 25, 75, "#217224"),
     new Player(Math.random() * canvas.width - 50, Math.random() * canvas.height, 50, 100, "#217224"),
     new Player(Math.random() * canvas.width - 75, Math.random() * canvas.height, 75, 125, "#217224"),
+    new Player(Math.random() * canvas.width - 25, Math.random() * canvas.height, 25, 75, "#217224"),
+    new Player(Math.random() * canvas.width - 50, Math.random() * canvas.height, 50, 100, "#217224"),
+    new Player(Math.random() * canvas.width - 75, Math.random() * canvas.height, 75, 125, "#217224"),
+    new Player(Math.random() * canvas.width - 25, Math.random() * canvas.height, 25, 75, "#217224"),
+    new Player(Math.random() * canvas.width - 50, Math.random() * canvas.height, 50, 100, "#217224"),
+    new Player(Math.random() * canvas.width - 75, Math.random() * canvas.height, 75, 125, "#217224"),
+    new Player(Math.random() * canvas.width - 25, Math.random() * canvas.height, 25, 75, "#217224"),
+    new Player(Math.random() * canvas.width - 50, Math.random() * canvas.height, 50, 100, "#217224"),
+    new Player(Math.random() * canvas.width - 75, Math.random() * canvas.height, 75, 125, "#217224"),
 ]
 const hut = new Player(0,0, 100, 100, "brown");
-const train = new Player(300, 300, 300, 100, "purple")
+const train = new Player(canvas.width - 320, canvas.height - 100, 300, 100, "purple")
 
 /* ----- FUNCTIONS ---------- */
 hut.enoughTime = true;
@@ -104,33 +116,28 @@ function gameloop() {
         hut.render();
     }
     for (let i = 0; i < trees.length; i++) {
-        // if (trees[i].enoughTime) {
-            //need to add functionality that skier can't move until the "auf geht's" button is clicked
-            if (currentlyPressedKeys["k"]) {
-                trees[i].y -= 10;
-            }
-            if (trees[i].y < 0 - trees[i].height) {
-                trees[i].y = canvas.height;
-            }  
-        // }  
-        trees[i].render();
+        //need to add functionality that skier can't move until the "auf geht's" button is clicked
+        if (currentlyPressedKeys["k"]) {
+            trees[i].y -= 10;
+        }
+        if (trees[i].y < 0 - trees[i].height) {
+            trees[i].y = canvas.height;
+        }
+        if (timeRemaining >= 5) {
+            trees[i].render();
+        }  
+    }
+    if (timeRemaining <= 5) {
+        train.render();
     }
     skier.render();
 }
 
-//at 0.5 seconds left on the timer, stop rendering trees and render the train station.
-
-
-/* ----- EVENT LISTENERS ---- */
-
-startButton.addEventListener("click", startCountdown);
-
 function startCountdown() {
     startButton.disabled = true;
-    let timeRemaining = 60;
     function updateTimer() {
-        const counterElement = document.getElementById("counter");
-        const timerElement = document.getElementById("timer");
+        // const counterElement = document.getElementById("counter");
+        // const timerElement = document.getElementById("timer");
         const minutes = Math.floor(timeRemaining / 60);
         const seconds = timeRemaining % 60;
         counterElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
@@ -147,6 +154,15 @@ function startCountdown() {
     }
     updateTimer();
 }
+
+
+/* ----- EVENT LISTENERS ---- */
+
+startButton.addEventListener("click", function() {
+    gameStarted = true;
+    startCountdown();
+});
+
 
 document.addEventListener('keydown', e => currentlyPressedKeys[e.key] = true);
 document.addEventListener('keyup', e => currentlyPressedKeys[e.key] = false);
